c89b8445f3acca1a6b01dd780864853a45f5595b
Manotosh-055 <manotoshkarmakar20512@gmail.com>
2024-06-09 23:24:50 +0530

    Changed in URL

--- ecommarce_backend/config/dbConnect.js
+++ ecommarce_backend/config/dbConnect.js
@@ -1,19 +1,37 @@
 import mongoose from "mongoose";
 
-const dbConnect = async () => {
-    try{
+// const dbConnect = async () => {
+//     try{
 
-        const conn = await mongoose.connect(process.env.MONGODB_URL,{
-            dbName:"apnamarket",
-            useNewUrlParser:true,
-            useUnifiedTopology:true
-        });
-        console.log("Database Connected Successfully");
-    }
-    catch(error){
-        console.log(error)
-        console.log("Error");
-    }
+//         const conn = await mongoose.connect(process.env.MONGODB_URL,{
+//             dbName:"apnamarket",
+//             useNewUrlParser:true,
+//             useUnifiedTopology:true
+//         });
+//         console.log("Database Connected Successfully");
+//     }
+//     catch(error){
+//         console.log(error)
+//         console.log("Error");
+//     }
    
+// }
+
+// For MongoDB Atlas
+// const DATABASE_URL = `process.env.MONGODB_URL`;
+const dbConnect = async() => {
+  try {
+      const DB_OPTIONS = {
+          dbName:"TechMarket",
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+      }
+      await mongoose.connect(process.env.MONGODB_URL, DB_OPTIONS)
+      console.log("Connected Sucessfully......")
+
+  } catch (error) {
+      console.log(error)
+  }
 }
+
 export default dbConnect;
